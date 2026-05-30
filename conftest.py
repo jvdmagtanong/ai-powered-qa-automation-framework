ccf8d24d61a66b2716532f040e96006296969cc7
jvdmagtanong <jvdmagtanong@gmail.com>
2026-05-29 19:56:01 -0700

    updated configurations to run with parameter for headless or headed
    added pytest.ini for pytest parameters
    updated conftest.py to accommodate option for headless or headed pytest

--- conftest.py
+++ conftest.py
@@ -1,10 +1,24 @@
 import pytest
 from playwright.sync_api import sync_playwright
 
+def pytest_addoption(parser):
+    parser.addoption(
+        "--headed",
+        action="store_true",
+        default=False,
+        help="Run browser in headed mode"
+    )
+
+
 @pytest.fixture
-def page():
+def page(request):
+    headed = request.config.getoption("--headed")
+
     with sync_playwright() as p:
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(
+            headless=not headed
+        )
+
         page = browser.new_page()
 
         yield page
